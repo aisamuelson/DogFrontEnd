63abc9c25e2fcfd73763b66cdc80a437c8f6e3b0
Aidan Samuelson <aidan.samuelson@wustl.edu>
2022-02-15 14:18:07 -0600

    Upload photos on Post,no luck with font,tried text

--- screens/TabThreeScreen.tsx
+++ screens/TabThreeScreen.tsx
@@ -2,11 +2,37 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import React, { useState, useEffect } from 'react';
+import { Button, Image, Platform } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { TextInput } from 'react-native-paper';
 
 export default function TabThreeScreen() {
+
+  const [image, setImage] = useState<string | null>();
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Post</Text>
+      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {/* <TextInput label='Email' value={this.state.text} onChangeText={text => this.setState({ text })}/> */}
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabThreeScreen.tsx" />
     </View>
